6d9bedf3ddb51508d3d8a270e5c2e0b4b7b55b49
Refik ÖNAL <refikonal@gmail.com>
2016-06-29 23:47:50 +0300

    bare boot

--- boot.php
+++ boot.php
@@ -1,1 +1,68 @@
-<?php //
+<?php
+
+// --------------------------------------------------------
+// src/laravel/bootstrap/autoload.php:
+// --------------------------------------------------------
+define('LARAVEL_START', microtime(true));
+
+$autoloader = require __DIR__ . '/src/vendor/autoload.php';
+
+$compiledPath = __DIR__ . '/src/laravel/bootstrap/cache/compiled.php';
+
+if (file_exists($compiledPath)) {
+    require $compiledPath;
+}
+
+
+// --------------------------------------------------------
+// src/laravel/bootstrap/app.php:
+// --------------------------------------------------------
+
+$app = new Illuminate\Foundation\Application(
+    realpath(__DIR__ . '/src/laravel/')
+);
+
+$app->singleton(
+    Illuminate\Contracts\Http\Kernel::class,
+    App\Http\Kernel::class
+);
+
+$app->singleton(
+    Illuminate\Contracts\Console\Kernel::class,
+    App\Console\Kernel::class
+);
+
+$app->singleton(
+    Illuminate\Contracts\Debug\ExceptionHandler::class,
+    App\Exceptions\Handler::class
+);
+
+
+// --------------------------------------------------------
+// src/laravel/public/index.php:
+// --------------------------------------------------------
+
+if ($app->runningInConsole()) {
+    // Load artisan kernel
+    $kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
+
+    $status = $kernel->handle(
+        $input = new Symfony\Component\Console\Input\ArgvInput,
+        new Symfony\Component\Console\Output\ConsoleOutput
+    );
+    $kernel->terminate($input, $status);
+
+    exit($status);
+
+} else {
+    // Continue with default http kernel
+    $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+    $response = $kernel->handle(
+        $request = Illuminate\Http\Request::capture()
+    );
+
+    $response->send();
+
+    $kernel->terminate($request, $response);
+}
